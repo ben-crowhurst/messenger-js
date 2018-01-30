9b97fa5c194af0ab6ef1d4788449babf27554bb1
Ben Crowhurst <ben.crowhurst@corvusoft.co.uk>
2018-01-30 22:08:37 +1100

    File shuffle.

--- src/main.js
+++ src/main.js
@@ -1,21 +1,8 @@
 'use strict';
 
+const router = require('./router');
+const cache = require('./data/cache');
 const configuration = require('./configuration');
-const parser = require('body-parser');
-const router = require('express')();
-router.use(parser.json({
-    type: 'application/json'
-}));
-router.use(parser.text({
-    type: 'text/html'
-}));
-
-const cache = require('node-persist');
-
-const messageController = require('./service/messageController').build(router, cache);
-const messagesController = require('./service/messagesController').build(router, cache);
-const attachmentController = require('./service/attachmentController').build(router, cache);
-const attachmentsController = require('./service/attachmentsController').build(router, cache);
 
 const service = router.listen(configuration.port, () => {
     cache.init().then(() => {
