69d7fc2f1d222b1ddf2f82aeb06d9553decca950
Ben Crowhurst <ben.crowhurst@corvusoft.co.uk>
2018-01-31 20:47:45 +1100

    First draft dispatch logic.

---
+++ src/dispatch/index.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const send = (messageKey, providers, cache, message) => {
+    let provider = providers.pop();
+    if (!provider) {
+        console.log('[Dispatch] All providers failed!');
+    }
+
+    provider.send(message).then((response) => {
+            cache.removeItem(messageKey, (error) => {
+                console.log('[Dispatch] Message sent with status: ', response.statusCode);
+
+                if (error) {
+                    console.log('[Dispatch] Failed to remove message from cache.');
+                }
+            });
+        })
+        .catch((error) => {
+            console.log('[Dispatch] Provider failed with status: ', error);
+            send(messageKey, providers, cache, message);
+        });
+};
+
+const dispatch = (messageKey, providers, cache) => {
+    cache.get(messageKey).then((message) => {
+        if (!message) {
+            return console.log('[Dispatch] Failed to locate message within the cache.');
+        }
+
+        send(messageKey, providers, cache, message);
+    })
+    .cache((error) => {
+        console.log('[Dispatch] Failed to load message ', messageKey);
+    });
+};
+
+module.exports = dispatch;
