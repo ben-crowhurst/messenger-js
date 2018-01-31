ca184c7c3930ade785f124b4ee453f43b20ea5bc
Ben Crowhurst <ben.crowhurst@corvusoft.co.uk>
2018-01-31 20:48:14 +1100

    Tide all components together.

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 'use strict';
 
-const cache = require('./data/cache');
-const router = require('./service/router');
+const cache = require('./data/cache'); //rename to index
+const dispatch = require('./dispatch');
+const providers = require('./providers');
 const configuration = require('./configuration');
+
+const router = require('./service/router');
+router.on('message-created', (messageKey) => {
+    let copyOfProviders = providers.slice();
+    dispatch(messageKey, copyOfProviders, cache); //, configuration); remove providiers constructor (providers also have delete, read etc...)
+});
 
 const service = router.listen(configuration.port, () => {
     cache.init().then(() => {
